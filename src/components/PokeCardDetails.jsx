124f08d2fcc5239b79c1e0783ff8a243144ad27d
efischer <etienne.fischer77@gmail.com>
2021-09-07 17:32:13 +0200

    [FIX] PokeCardDetails: display sprites if gifs are unavailable, or default picture if none is available

--- src/components/PokeCardDetails.jsx
+++ src/components/PokeCardDetails.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { withRouter } from "react-router";
+import defaultPicture from "../assets/quel_est_ce_pokemon.jpg";
 
 import "./PokeCardDetails.css";
 
@@ -16,49 +17,89 @@ const PokeCardDetails = ({ details }) => {
   return (
     <div className={`details-card ${"details-" + details.types[0].type.name}`}>
       <h2 className="details-name">{details.name.toUpperCase()}</h2>
-      <div className="details-img">
-        <img
-          className="details-gif"
-          src={
-            details.sprites.versions["generation-v"]["black-white"].animated
-              .front_default
-          }
-          alt="front default"
-        />
-        <img
-          className="details-gif"
-          src={
-            details.sprites.versions["generation-v"]["black-white"].animated
-              .back_default
-          }
-          alt="back default"
-        />
-        <img
-          className="details-gif"
-          src={
-            details.sprites.versions["generation-v"]["black-white"].animated
-              .front_shiny
-          }
-          alt="front shiny"
-        />
-        <img
-          className="details-gif"
-          src={
-            details.sprites.versions["generation-v"]["black-white"].animated
-              .back_shiny
-          }
-          alt="back shiny"
-        />
-      </div>
-      <div className="details-tall">
-        <p>Height : {details.height / 10} m</p>
-        <p>Weight : {details.weight / 10} kg</p>
+      <div className="details-sprites">
+        {details.sprites.versions["generation-v"]["black-white"].animated
+          .front_default ? (
+          <>
+            <img
+              className="details-gif"
+              src={
+                details.sprites.versions["generation-v"]["black-white"].animated
+                  .front_default
+              }
+              alt="front default"
+            />
+            <img
+              className="details-gif"
+              src={
+                details.sprites.versions["generation-v"]["black-white"].animated
+                  .back_default
+              }
+              alt="back default"
+            />
+            <img
+              className="details-gif"
+              src={
+                details.sprites.versions["generation-v"]["black-white"].animated
+                  .front_shiny
+              }
+              alt="front shiny"
+            />
+            <img
+              className="details-gif"
+              src={
+                details.sprites.versions["generation-v"]["black-white"].animated
+                  .back_shiny
+              }
+              alt="back shiny"
+            />
+          </>
+        ) : (
+          <>
+            <img
+              className="details-img"
+              src={
+                details.sprites.versions["generation-v"]["black-white"]
+                  .front_default || defaultPicture
+              }
+              alt="front default"
+            />
+            <img
+              className="details-img"
+              src={
+                details.sprites.versions["generation-v"]["black-white"]
+                  .back_default || defaultPicture
+              }
+              alt="back default"
+            />
+            <img
+              className="details-img"
+              src={
+                details.sprites.versions["generation-v"]["black-white"]
+                  .front_shiny || defaultPicture
+              }
+              alt="front shiny"
+            />
+            <img
+              className="details-img"
+              src={
+                details.sprites.versions["generation-v"]["black-white"]
+                  .back_shiny || defaultPicture
+              }
+              alt="back shiny"
+            />
+          </>
+        )}
       </div>
       <div className="types">
         <p>Type 1: {details.types[0].type.name}</p>
         {details.types.length === 2 && (
           <p>Type 2: {details.types[1].type.name}</p>
         )}
+      </div>
+      <div className="details-tall">
+        <p>Height : {details.height / 10} m</p>
+        <p>Weight : {details.weight / 10} kg</p>
       </div>
       <div className="stats">
         <p>Stats :</p>
