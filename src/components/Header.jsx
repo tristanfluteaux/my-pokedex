5c1ebe802b88d5da0234b1efe9257a6fd1467ffe
Tristan <tristan.fluteaux@gmail.com>
2021-09-18 17:33:02 +0200

    [STYLE] responsive css for details and button css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,7 @@ const Header = () => {
     return (
         <div className='main-header'>
             <NavLink className='header-navlink' to='/'><h1 className='header-title'>Pokédex</h1></NavLink>
-            <img style={{width: '150px'}} src='https://i.pinimg.com/originals/38/fb/9b/38fb9b66a2636f1b7dcb9ea68d290c05.gif' />
-            <img style={{width: '150px'}} src='http://25.media.tumblr.com/tumblr_m5zyheRG0l1qj7ct2o1_500.gif' />
-            <img style={{width: '150px'}} src='https://c.tenor.com/vYhrJmhMswcAAAAi/pokemon-snorlax.gif' />
+            <img className='head-gif' src='https://c.tenor.com/vYhrJmhMswcAAAAi/pokemon-snorlax.gif' />
         </div>
     )
 }
