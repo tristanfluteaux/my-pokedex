df48c17f1a0d93553455c7ddbbbc44f8d1f55ec0
efischer <etienne.fischer77@gmail.com>
2021-10-07 15:51:14 +0200

    [STYLE] card shadows in list + search bar autocompletion

--- src/screens/Search.jsx
+++ src/screens/Search.jsx
@@ -2,54 +2,56 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
-import '../components/Header.css';
+import "../components/Header.css";
 
-const Search = ({shiny}) => {
+const Search = ({ shiny }) => {
+  const [searchValue, setSearchValue] = useState("");
+  const [name, setName] = useState([]);
 
-    const [searchValue, setSearchValue] = useState('');
-    const [name, setName] = useState([]);
-
-    useEffect(() => {
-        const getSearch = () => {
-        axios
-            .get('https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0')
-            .then((res) => setName(res.data.results))
-        }
-        getSearch();
-    }, [searchValue])
-
-    const handleChange = (event) => {
-        setSearchValue(event.target.value)
+  useEffect(() => {
+    const getSearch = () => {
+      axios
+        .get("https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0")
+        .then((res) => setName(res.data.results));
     };
+    getSearch();
+  }, [searchValue]);
 
-    return (
-        <div className={`parents-search ${shiny ? "list-background" : ""}`}>
-            <input
-            value={searchValue}
-            onChange={handleChange}
-            type='text'
-            placeholder='Search a Pokemon ?'
-            id='search-input'
-            />
+  const handleChange = (event) => {
+    setSearchValue(event.target.value);
+  };
 
-            <div className='search-result'>
-                <ul className='result-ul'>
-                {searchValue && name && 
-                name
-                .filter(word => word.name.match(searchValue))
-                .map((poke) => (
+  return (
+    <div className={`parents-search ${shiny ? "list-background" : ""}`}>
+      <input
+        value={searchValue}
+        onChange={handleChange}
+        type="text"
+        placeholder="Search a Pokemon ?"
+        id="search-input"
+        autoComplete="off"
+      />
+
+      <div className="search-result">
+        <ul className="result-ul">
+          {searchValue &&
+            name &&
+            name
+              .filter((word) => word.name.match(searchValue))
+              .map((poke) => (
                 <li key={poke.name}>
-                    <NavLink
-                    className='suggest'
-                    to={`/pokemon/${poke.url.split('/')[6]}`}>
+                  <NavLink
+                    className="suggest"
+                    to={`/pokemon/${poke.url.split("/")[6]}`}
+                  >
                     {poke.name}
-                    </NavLink>
+                  </NavLink>
                 </li>
-                ))}
-                </ul>
-            </div>
-        </div>
-    )
-}
+              ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Search;
