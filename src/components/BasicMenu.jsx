32a2d61e4caea89479e3fccb3b238a2875510699
Tristan <tristan.fluteaux@gmail.com>
2021-10-15 00:00:34 +0200

    [STYLE] Add material ui icons on footer and menu, responsive css for footer and evolution-chain on details

---
+++ src/components/BasicMenu.jsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { NavLink } from 'react-router-dom';
+
+import Feunard from '../assets/Feunard.png'
+
+export default function BasicMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls="basic-menu"
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        
+      >
+        <img src={Feunard} alt='' style={{width:'100px'}}/>
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}><NavLink to='/team'>Team</NavLink></MenuItem>
+        <MenuItem onClick={handleClose}><NavLink to='/'>MainList</NavLink></MenuItem>
+        <MenuItem onClick={handleClose}>Troll</MenuItem>
+      </Menu>
+    </div>
+  );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import { Dropdown } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 import "./Header.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import BasicMenu from "./BasicMenu";
 
 const Header = () => {
   return (
@@ -10,28 +10,7 @@ const Header = () => {
       <NavLink className="header-navlink" to="/">
         <h1 className="header-title">Pokédex</h1>
       </NavLink>
-      <div style={{ paddingTop: "20vh" }}>
-        <Dropdown>
-          <Dropdown.Toggle
-            id="dropdown-button-dark-example1"
-            variant="secondary"
-          >
-            Menu
-          </Dropdown.Toggle>
-          <Dropdown.Menu variant="dark">
-            <Dropdown.Item active>
-              <NavLink
-                to="/team"
-                style={{ listStyleType: "none", color: "white" }}
-              >
-                Team
-              </NavLink>
-            </Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item>Troll</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </div>
+      <BasicMenu />
     </div>
   );
 };
