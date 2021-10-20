a9dfe932c7ee1468f875b17e430c850e87b2112a
Tristan <tristan.fluteaux@gmail.com>
2021-10-20 10:59:50 +0200

    {FEAT} add alert style on favorites and new navmenu

---
+++ src/components/Menu/NavMenu.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+
+import './NavMenu.css'
+
+const NavMenu = () => {
+    const [isOpen, setIsOpen] = useState()
+    const handleClick = () => {
+        setIsOpen(!isOpen)
+    }
+    return (  
+        <div className='mobile-navbar'>
+        {isOpen && (
+          <div className='mobile-menu'>
+            <NavLink onClick={handleClick} to='/'>
+              Pokedex
+            </NavLink>
+            <NavLink onClick={handleClick} to='/team'>
+              Team
+            </NavLink>
+            <NavLink onClick={handleClick} to='/troll'>
+              Troll
+            </NavLink>
+          </div>
+        )}
+  
+        <div onClick={handleClick} className={`bar-container`}>
+          <span className={`close-bar ${isOpen && 'open-bar'}`}></span>
+          <span className={`close-bar ${isOpen && 'open-bar'}`}></span>
+          <span className={`close-bar ${isOpen && 'open-bar'}`}></span>
+        </div>
+      </div>
+    );
+}
+
+export default NavMenu;
