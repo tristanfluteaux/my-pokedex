0bd7575d9cbcb78e7ad081c3d15a21334eedce16
efischer <etienne.fischer77@gmail.com>
2021-10-21 19:01:04 +0200

    [REFACTO] load all pokemon and pokemon datas at top of the app (not working)

--- src/App.js
+++ src/App.js
@@ -7,9 +7,36 @@ import useFavoris from "./components/useFavoris";
 import Footer from "./components/Footer";
 
 import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [pokemon, setPokemon] = useState();
+  // const [pokemonData, setPokemonData] = useState();
   const [favorites, toogleFavorite] = useFavoris();
+
+  useEffect(() => {
+    const getPokemon = () => {
+      axios
+        .get("https://pokeapi.co/api/v2/pokemon?limit=2000")
+        .then((res) => setPokemon(res.data.results));
+    };
+    getPokemon();
+  }, []);
+
+  useEffect(() => {
+    if (pokemon) {
+      console.log(
+        pokemon
+          .map(async (pokemon) => {
+            const { data } = await axios.get(pokemon.url);
+            return data;
+          })
+          .map((promise) => promise.then((res) => res))
+      );
+    }
+  }, [pokemon]);
 
   return (
     <div className="App">
