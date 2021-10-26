c35a2ec82223ffc94f9844e267e50c8aeeae2156
efischer <etienne.fischer77@gmail.com>
2021-10-26 12:30:01 +0200

    [REFACTO] get api's data at page first loading

--- src/App.js
+++ src/App.js
@@ -14,53 +14,76 @@ import Troll from "./components/Troll/Troll";
 import TrollEnter from "./components/Troll/TrollEnter";
 
 function App() {
-  const [pokemon, setPokemon] = useState();
-  // const [pokemonData, setPokemonData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [pokemonArray, setPokemonArray] = useState();
+  const [pokemonData, setPokemonData] = useState();
   const [favorites, toogleFavorite] = useFavoris();
 
   useEffect(() => {
-    const getPokemon = () => {
-      axios
+    const getPokemon = async () => {
+      await axios
         .get("https://pokeapi.co/api/v2/pokemon?limit=2000")
-        .then((res) => setPokemon(res.data.results));
+        .then(async (res) => {
+          await setPokemonArray(res.data.results);
+        });
     };
     getPokemon();
   }, []);
 
   useEffect(() => {
-    if (pokemon) {
-      console.log(
-        pokemon
-          .map(async (pokemon) => {
-            const { data } = await axios.get(pokemon.url);
-            return data;
-          })
-          .map((promise) => promise.then((res) => res))
-      );
+    if (pokemonArray) {
+      const promiseArray = [];
+      const data = [];
+
+      const getPromise = async (url) => {
+        return await promiseArray.push(axios.get(url));
+      };
+
+      const getPromiseArray = async () => {
+        return Promise.all(
+          pokemonArray.map((pokemon) => getPromise(pokemon.url))
+        );
+      };
+
+      getPromiseArray().then(async () => {
+        await Promise.all(
+          promiseArray.map((promise) =>
+            promise.then((res) => data.push(res.data))
+          )
+        );
+        setPokemonData(data);
+        setLoading(false);
+      });
     }
-  }, [pokemon]);
+  }, [pokemonArray]);
+
+  if (!loading) console.log(pokemonData);
 
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
-        <Switch>
-          <Route exact path="/">
-            <Home favorites={favorites} toogleFavorite={toogleFavorite} />
-          </Route>
-          <Route path="/pokemon/:id">
-            <PokeDetails />
-          </Route>
-          <Route exact path="/team">
-            <Team favorites={favorites} toogleFavorite={toogleFavorite} />
-          </Route>
-          <Route exact path='/troll'>
-            <TrollEnter/>
-          </Route>
-          <Route exact path="/trollreal">
-            <Troll/>
-          </Route>
-        </Switch>
+        {loading ? (
+          <p>loading...</p>
+        ) : (
+          <Switch>
+            <Route exact path="/">
+              <Home favorites={favorites} toogleFavorite={toogleFavorite} />
+            </Route>
+            <Route path="/pokemon/:id">
+              <PokeDetails />
+            </Route>
+            <Route exact path="/team">
+              <Team favorites={favorites} toogleFavorite={toogleFavorite} />
+            </Route>
+            <Route exact path="/troll">
+              <TrollEnter />
+            </Route>
+            <Route exact path="/trollreal">
+              <Troll />
+            </Route>
+          </Switch>
+        )}
         <Footer />
       </BrowserRouter>
     </div>
